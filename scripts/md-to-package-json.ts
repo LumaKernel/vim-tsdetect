8bd61bc7e5f0ad8c3422711113a1069d1e6dacee
Luma <world@luma.email>
2021-03-06 22:34:15 +0900

    ci: check conversion check and moderate package.json npm scripts

--- scripts/md-to-package-json.ts
+++ scripts/md-to-package-json.ts
@@ -1,8 +1,14 @@
 import path from "path";
 import fs from "fs";
 import { execFileSync } from "child_process";
+import equal from "deep-equal";
 import { parseReadmeMd } from "../src/utils/readme";
 import { applyOptionDeclarations } from "../src/utils/package-json";
+
+const exitCode = process.argv.slice(1).includes("--exit-code");
+if (exitCode) {
+  console.info("Exit code mode is on.");
+}
 
 const readmeMdPath = path.resolve(__dirname, "../README.md");
 const packageJsonPath = path.resolve(__dirname, "../package.json");
@@ -17,9 +23,18 @@ const appliedJson = applyOptionDeclarations(
   packageJsonContent as any,
   parseReadme,
 );
-fs.writeFileSync(
-  packageJsonPath,
-  JSON.stringify(appliedJson as any, null, "  "),
-);
-
-execFileSync("yarn", ["fix", "./package.json"]);
+//
+if (exitCode) {
+  if (!equal(appliedJson, packageJsonContent)) {
+    console.error("md-to-package-json.ts is not applied.");
+    process.exit(1);
+  }
+} else {
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(appliedJson as any, null, "  "),
+  );
+  execFileSync("yarn", ["eslint", "--fix", packageJsonPath], {
+    stdio: "inherit",
+  });
+}
