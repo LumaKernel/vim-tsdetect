c16c54508697ecce81e1fbdc515ec9a4145687a2
Luma <world@luma.email>
2021-03-06 17:59:19 +0900

    refactor: globals.d.ts

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,3 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference -- support Node.js 12 */
-/// <reference path="./node_modules/better-typescript-lib/lib.es5.d.ts" />
-/// <reference path="./node_modules/better-typescript-lib/lib.es2015.d.ts" />
 /// <reference path="./node_modules/better-typescript-lib/lib.es2019.d.ts" />
 /* eslint-enable @typescript-eslint/triple-slash-reference */
