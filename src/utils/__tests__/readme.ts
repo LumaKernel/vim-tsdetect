cac209bbd5919bd67dd32cae273508dd3bd71511
Luma <world@luma.email>
2021-03-06 20:29:20 +0900

    build: create documentations generator; README.md to package.json

---
+++ src/utils/__tests__/readme.ts
@@ -0,0 +1,54 @@
+import { parseReadmeMd } from "../readme";
+
+describe("parseReadmeMd", () => {
+  it("should parse README Options section", () => {
+    expect(
+      parseReadmeMd([
+        "# Hi",
+        "",
+        "## Foo",
+        "",
+        "Blah, blah.",
+        "",
+        "## Options",
+        "",
+        "### `extension.a`",
+        "",
+        "This is...",
+        "",
+        "Values: `number`",
+        "Default:",
+        "```json",
+        "{",
+        '  "extension.a": 0',
+        "}",
+        "```",
+        "",
+        "### `extension.b`",
+        "B is a good option.",
+        "You should use this.",
+        "",
+        "NOTE: Be careful to use this.",
+        "Values: `number`",
+        "## Some",
+      ]),
+    ).toEqual([
+      {
+        descriptionLines: ["This is..."],
+        optionName: "extension.a",
+      },
+      {
+        descriptionLines: [
+          "B is a good option.",
+          "You should use this.",
+          "",
+          "NOTE: Be careful to use this.",
+        ],
+        optionName: "extension.b",
+      },
+    ]);
+  });
+  it("should throw for illegal heading", () => {
+    expect(() => parseReadmeMd(["## Options", "### extension.a"])).toThrow();
+  });
+});
---
+++ src/utils/package-json.ts
@@ -0,0 +1,51 @@
+import type { OptionDeclaration } from "./readme";
+
+export type ContributesConfigurationsConfiguration = {
+  type: string;
+  default: unknown;
+  markdownDescription?: string;
+};
+
+export type ContributesConfigurations = {
+  type: string;
+  title: string;
+  properties: {
+    [optionName: string]: ContributesConfigurationsConfiguration;
+  };
+};
+
+export type PackageJsonContributesConfigurations = {
+  contributes: {
+    configuration: ContributesConfigurations;
+  };
+};
+
+export const applyOptionDeclarations = (
+  json: Readonly<PackageJsonContributesConfigurations>,
+  optionDeclarations: OptionDeclaration[],
+): PackageJsonContributesConfigurations => {
+  const properties = { ...json.contributes.configuration.properties };
+  optionDeclarations.forEach((declaration) => {
+    const property = properties[declaration.optionName];
+    if (!property) {
+      throw new Error(
+        `Option ${declaration.optionName} in README does not exist in package.json.`,
+      );
+    }
+    properties[declaration.optionName] = {
+      ...property,
+      markdownDescription: declaration.descriptionLines.join("\n"),
+    };
+  });
+
+  return {
+    ...json,
+    contributes: {
+      ...json.contributes,
+      configuration: {
+        ...json.contributes.configuration,
+        properties,
+      },
+    },
+  };
+};
