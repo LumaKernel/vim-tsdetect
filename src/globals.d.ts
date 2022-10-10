6632a8253af28b17ea0909941cc506f74620bed3
Luma <world@luma.email>
2022-10-10 21:25:29 +0900

    feat!: Support coc.nvim latest(0.0.82) and drop prior versions
    - Support 0.0.82
    - Drop support for old coc.nvim versions. (prior to 0.0.82)
    - Drop Node.js v10 because coc.nvim also drops.
    - Fix for errors on switching to node but you should use patched
      version. Refer to https://github.com/fannheyward/coc-deno/issues/184

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'coc.nvim' {
-  import type { ConfigurationTarget } from './coc_internal';
-
-  export namespace workspace {
-    export function getConfigFile(target: ConfigurationTarget): string | undefined;
-  }
-}
